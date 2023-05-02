70932ee4d66b7eefdc4ed0fbf58f630924d6cc31
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-02 15:56:23 -0700

    displays the dropdown on click

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { AiOutlineDown } from "react-icons/ai";
 
 export default function DropDown() {
+  const [showDropDown, setShowDropDown] = useState(false);
+
+  const dropdownHandler = () => {
+    setShowDropDown((prev) => !prev);
+  };
+
   return (
-    <div className='flex w-auto justify-between border border-black rounded-lg'>
-      <span className='flex justify-start pl-4 pr-4'>Select an option</span>
-      <span className='flex justify-center items-center pr-1'>
-        <AiOutlineDown />
-      </span>
-    </div>
+    <>
+      <div
+        onClick={dropdownHandler}
+        className="flex w-auto justify-between border border-black rounded-lg"
+      >
+        <span className="flex justify-start pl-4 pr-4">Select an option</span>
+        <span className="flex justify-center items-center pr-1">
+          <AiOutlineDown />
+        </span>
+      </div>
+      {showDropDown && <div>This is working</div>}
+    </>
   );
 }
