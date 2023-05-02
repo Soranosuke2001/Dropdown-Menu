34f14e61ce8b9d7460de62afdd9f9f4f43903412
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-02 15:38:11 -0700

    starter files

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className='h-screen w-screen flex fex-col justify-center items-center'>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p>Testing</p>
+    </div>
+  );
+}
