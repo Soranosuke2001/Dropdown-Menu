5edcff89267505df83424784bbaad60206f5a434
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-03 15:14:43 -0700

    dropdown menu options component

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import { AiOutlineDown } from "react-icons/ai";
+import MenuOptions from "./MenuOptions";
 
 export default function DropDown() {
   const [showDropDown, setShowDropDown] = useState(false);
@@ -13,14 +14,14 @@ export default function DropDown() {
     <>
       <div
         onClick={dropdownHandler}
-        className="flex w-auto justify-between border border-black rounded-lg"
+        className="flex w-auto justify-between border border-black rounded-lg hover:cursor-pointer"
       >
         <span className="flex justify-start pl-4 pr-4">Select an option</span>
         <span className="flex justify-center items-center pr-1">
           <AiOutlineDown />
         </span>
       </div>
-      {showDropDown && <div>This is working</div>}
+      {showDropDown && <MenuOptions />}
     </>
   );
 }
