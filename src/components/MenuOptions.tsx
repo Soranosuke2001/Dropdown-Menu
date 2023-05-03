686c309a9deaf2ce4b76423a70fdd23fc532ef32
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-03 15:33:59 -0700

    renders a list of options with checkboxes

--- src/components/MenuOptions.tsx
+++ src/components/MenuOptions.tsx
@@ -3,11 +3,18 @@ import React from "react";
 import { Options } from "../lib/Options";
 
 export default function MenuOptions() {
-  return <>
-    <div className='border border-black text-red-600'>
-        {Options.map((option) => (
-            <div>{option}</div>
-        ))}
-    </div>
-  </>;
+  return (
+    <>
+      <div className="border border-black text-red-600">
+          {Options.map((option, index) => (
+            <>
+                <label key={index}>
+                    <input type='checkbox' value={option} />
+                    {option}
+                </label>
+            </>
+          ))}
+      </div>
+    </>
+  );
 }
