27103d8a344d7acad3318300e188eb1d121b8378
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-03 15:19:09 -0700

    displays the list of options

--- src/components/MenuOptions.tsx
+++ src/components/MenuOptions.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { Options } from "../lib/Options";
+
 export default function MenuOptions() {
   return <>
     <div className='border border-black text-red-600'>
-        Menu Options
+        {Options.map((option) => (
+            <div>{option}</div>
+        ))}
     </div>
   </>;
 }
---
+++ src/lib/Options.ts
@@ -0,0 +1,16 @@
+export const Options = [
+    "Action",
+    "Adventure",
+    "Comedy",
+    "Crime & Mystery",
+    "Fantasy",
+    "Historical",
+    "Horror",
+    "Romance",
+    "Satire",
+    "Science Fiction",
+    "Speculative",
+    "Thriller",
+    "Isekai",
+    "Other"
+]
