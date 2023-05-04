1843dc4899b0ef2a94e89c2bc8077d27020b314d
sora <sorazora1.4@gmail.com>
2023-05-03 20:27:34 -0700

    reads teh local storage state

--- src/components/MenuOptions.tsx
+++ src/components/MenuOptions.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 interface MenuOptionsProps {
-  optionChecked: any;
+  optionChecked: {};
   checkboxHandler: (genre: string) => void;
 }
 
 export default function MenuOptions(props: MenuOptionsProps) {
   const { optionChecked, checkboxHandler } = props;
-  const optionList = Object.keys(optionChecked)
+  const optionList = Object.keys(optionChecked);
+
   return (
     <>
       <div className="border border-black rounded-lg p-3">
@@ -15,12 +16,22 @@ export default function MenuOptions(props: MenuOptionsProps) {
           <div key={index} className="p-2">
             <label className="flex gap-2">
               <span>
-                <input
-                  type="checkbox"
-                  value={option}
-                  className="hover:cursor-pointer"
-                  onClick={(e) => checkboxHandler(e.currentTarget.value)}
-                />
+                {optionChecked[`${option}`] ? (
+                  <input
+                    type="checkbox"
+                    value={option}
+                    className="hover:cursor-pointer"
+                    onClick={(e) => checkboxHandler(e.currentTarget.value)}
+                    checked
+                  />
+                ) : (
+                  <input
+                    type="checkbox"
+                    value={option}
+                    className="hover:cursor-pointer"
+                    onClick={(e) => checkboxHandler(e.currentTarget.value)}
+                  />
+                )}
               </span>
               <span>{option}</span>
             </label>
